8a6ad584b7f56b0b7469ce3d0365500ede5a4af2
Eugene Nakoneschniy <evgeniy.nakoneschniy@suttermills.com>
2018-09-19 10:25:58 +0300

    initial project + routing

---
+++ router.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '~/pages/index'
+import About from '~/pages/about'
+
+Vue.use(Router);
+
+export function createRouter() {
+  return new Router({
+    mode: 'history',
+    routes: [
+      {
+        name: 'home',
+        path: '/',
+        component: Home
+      },
+      {
+        name: 'about',
+        path: '/company/about-us',
+        component: About
+      }
+    ]
+  })
+}
