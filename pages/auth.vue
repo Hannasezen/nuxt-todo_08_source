cc5bdd84540501c1d7b9f889e06efa8b32d7b0c9
Hannasezen <andreeva0649@gmail.com>
2018-10-03 18:18:29 +0300

    "auth page"

---
+++ pages/auth.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+<script>
+export default {
+    name: 'auth'
+}
+</script>
+<style scoped>
+
+</style>
+
+
--- router.js
+++ router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '~/pages/index'
 import About from '~/pages/about'
+import Auth from '~/pages/auth'
 
 Vue.use(Router);
 
@@ -18,6 +19,11 @@ export function createRouter() {
         name: 'about',
         path: '/about',
         component: About
+      },
+      {
+        name: 'auth',
+        path: '/auth',
+        component: Auth
       }
     ]
   })
